044df4cf1d129b6601e19f52f928373b759e722a
valentina-rojas <valentinarojas618@gmail.com>
2025-04-06 18:43:57 -0300

    on mouse enter libros

--- Assets/Scripts/BookData.cs
+++ Assets/Scripts/BookData.cs
@@ -2,15 +2,34 @@ using UnityEngine;
 
 public class BookData : MonoBehaviour
 {
-
     public int libroID;
     public string tipoLibro;
     public string titulo;
     public string descripcion;
     public Sprite imagenLibro;
 
+
+    private SpriteRenderer spriteRenderer;
+    private Color originalColor;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        originalColor = spriteRenderer.color;
+    }
+
     private void OnMouseDown()
     {
         BookManager.instance.MostrarInformacion(this);
+    }
+
+    private void OnMouseEnter()
+    {
+        spriteRenderer.color = new Color(originalColor.r * 0.7f, originalColor.g * 0.7f, originalColor.b * 0.7f);
+    }
+
+    private void OnMouseExit()
+    {
+        spriteRenderer.color = originalColor;
     }
 }
