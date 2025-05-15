7603c03fa46c96b4f7cebb7b326d14d3aa2fbbe0
SolRomero1999 <128836209+SolRomero1999@users.noreply.github.com>
2025-05-15 16:38:24 -0300

    Pasaje de libros de una misma sección

--- Assets/Scripts/BookManager.cs
+++ Assets/Scripts/BookManager.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Collections.Generic;
+using System.Linq;
 
 public class BookManager : MonoBehaviour
 {
@@ -17,50 +19,109 @@ public class BookManager : MonoBehaviour
     public Image imagenConfirmarSeleccion;
     public TMP_Text tituloConfirmarSeleccion;
 
+    public Button botonConfirmar;
+    public Button botonSiguiente;  
+    public Button botonAnterior; 
+
     private CharacterSpawn characterSpawn;
 
-    public Button botonConfirmar;
-
+    private List<BookData> librosMismaSeccion = new List<BookData>(); 
+    private int indiceLibroActual = 0; 
 
     private void Awake()
     {
         if (instance == null)
-        {
             instance = this;
-        }
         else
-        {
             Destroy(gameObject);
-        }
 
         characterSpawn = FindFirstObjectByType<CharacterSpawn>();
         if (characterSpawn == null)
-        {
             Debug.LogError("CharacterSpawn no encontrado por BookManager.");
-        }
     }
-
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            if (panelInfoLibro.activeSelf)
-            {
-                CancelarSeleccion();
-            }
-        }
+        if (Input.GetKeyDown(KeyCode.Escape) && panelInfoLibro.activeSelf)
+            CancelarSeleccion();
     }
-
 
     public void MostrarInformacion(BookData libro)
     {
         libroActual = libro;
 
+        librosMismaSeccion.Clear();
+        BookData[] todosLosLibros = FindObjectsByType<BookData>(FindObjectsSortMode.None);
+
+        foreach (BookData b in todosLosLibros)
+        {
+            if (!b.gameObject.activeInHierarchy || b.tipoLibro != libro.tipoLibro)
+                continue;
+
+            Transform parentEstante = b.transform.parent;
+            ShelfSlots estante = parentEstante != null ? parentEstante.GetComponent<ShelfSlots>() : null;
+
+            if (estante != null && estante.generoPermitido == b.tipoLibro)
+            {
+                librosMismaSeccion.Add(b);
+            }
+        }
+
+        librosMismaSeccion = librosMismaSeccion
+            .OrderBy(b => b.transform.position.x) 
+            .ToList();
+
+        indiceLibroActual = librosMismaSeccion.IndexOf(libro);
+
+        if (indiceLibroActual == -1)
+        {
+            libroActual = libro;
+            panelInfoLibro.SetActive(true);
+            tituloTexto.text = libroActual.titulo;
+            descripcionTexto.text = libroActual.descripcion;
+            imagenLibroUI.sprite = libroActual.imagenLibro;
+
+            botonAnterior.interactable = false;
+            botonSiguiente.interactable = false;
+        }
+        else
+        {
+            MostrarLibroPorIndice(indiceLibroActual);
+        }
+
+    }
+
+    private void MostrarLibroPorIndice(int indice)
+    {
+        if (indice < 0 || indice >= librosMismaSeccion.Count) return;
+
+        libroActual = librosMismaSeccion[indice];
+
         panelInfoLibro.SetActive(true);
-        tituloTexto.text = libro.titulo;
-        descripcionTexto.text = libro.descripcion;
-        imagenLibroUI.sprite = libro.imagenLibro;
+        tituloTexto.text = libroActual.titulo;
+        descripcionTexto.text = libroActual.descripcion;
+        imagenLibroUI.sprite = libroActual.imagenLibro;
+
+        botonAnterior.interactable = indice > 0;
+        botonSiguiente.interactable = indice < librosMismaSeccion.Count - 1;
+    }
+
+    public void VerSiguienteLibro()
+    {
+        if (indiceLibroActual < librosMismaSeccion.Count - 1)
+        {
+            indiceLibroActual++;
+            MostrarLibroPorIndice(indiceLibroActual);
+        }
+    }
+
+    public void VerLibroAnterior()
+    {
+        if (indiceLibroActual > 0)
+        {
+            indiceLibroActual--;
+            MostrarLibroPorIndice(indiceLibroActual);
+        }
     }
 
     public void ConfirmarSeleccion()
@@ -77,8 +138,6 @@ public class BookManager : MonoBehaviour
         panelConfirmarSeleccion.SetActive(true);
         imagenConfirmarSeleccion.sprite = libroActual.imagenLibro;
         tituloConfirmarSeleccion.text = libroActual.titulo;
- 
-
     }
 
     public void HabilitarBotonConfirmacion()
@@ -102,9 +161,6 @@ public class BookManager : MonoBehaviour
         GameManager.instance.VerificarRecomendacion(libroActual);
 
         if (characterSpawn != null)
-        {
             characterSpawn.EndInteraction();
-        }
-
     }
 }
