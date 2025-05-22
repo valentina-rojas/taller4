7e0efd499cdae233f583ed4ed0d73163815bea7d
valentina-rojas <valentinarojas618@gmail.com>
2025-05-22 16:06:23 -0300

    ajustes volumen

---
+++ Assets/Scripts/MenuAjustes.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+
+public class MenuAjustes : MonoBehaviour
+{
+    public Slider musicSlider;
+    public AudioMixer audioMixer;
+
+    void Start()
+    {
+        musicSlider.value = 0.5f;
+        SetMusicVolume(musicSlider.value);
+        musicSlider.onValueChanged.AddListener(SetMusicVolume);
+    }
+
+
+    public void SetMusicVolume(float volume)
+    {
+        float minVolume = 0.0001f;
+      audioMixer.SetFloat("MusicVolume", Mathf.Log10(Mathf.Max(volume, minVolume)) * 20);
+    }
+}
